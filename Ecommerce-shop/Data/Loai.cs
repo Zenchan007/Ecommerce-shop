a41bee03a841848c156a399916fe4772b9997bc3
Tuấn Nguyễn <tuannguyenvan299002@gmail.com>
2023-12-29 11:38:38 +0700

    Khởi tạo project, generate data, cấu hình connection string

---
+++ Ecommerce-shop/Data/Loai.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ecommerce_shop.Data
+{
+    public partial class Loai
+    {
+        public Loai()
+        {
+            HangHoas = new HashSet<HangHoa>();
+        }
+
+        public int MaLoai { get; set; }
+        public string TenLoai { get; set; } = null!;
+        public string? TenLoaiAlias { get; set; }
+        public string? MoTa { get; set; }
+        public string? Hinh { get; set; }
+
+        public virtual ICollection<HangHoa> HangHoas { get; set; }
+    }
+}
