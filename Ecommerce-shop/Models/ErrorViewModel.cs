647f501deeff48df23bafdb24ccb278cc6e631d2
Tuấn Nguyễn <tuannguyenvan299002@gmail.com>
2023-12-29 10:36:55 +0700

    Add project files.

---
+++ Ecommerce-shop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce_shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
